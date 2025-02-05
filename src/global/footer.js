cf461dbea2eca7c7adb7c5933b47f5754750391b
Soma Szoboszlai <somaszoboszlai@gmail.com>
2025-02-05 23:22:13 +0100

    update code

--- src/global/footer.js
+++ src/global/footer.js
@@ -1,7 +1,11 @@
 // import { gsap } from "gsap";
 
 function footer() {
-
+        const yearElement = document.querySelector("#year");
+        if (yearElement) {
+          yearElement.textContent = new Date().getFullYear();
+        }
+      
 }
 
 export default footer
